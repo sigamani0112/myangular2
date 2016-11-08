f43332bc4ac4b085bdde4dfe28f2b25fd88dc85f
Deivasigamani M <siga.locus@gmail.com>
2016-11-08 15:56:34 +0530

    After the Http implementation

--- app/form1.component.ts
+++ app/form1.component.ts
@@ -1,5 +1,8 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 import {MyMoneyRecord} from './myMoneyRecord';
+
+import {MyMoneyRecordService} from './myMoneyRecord.service';
+
 
 @Component({
   moduleId: module.id,  
@@ -7,24 +10,52 @@ import {MyMoneyRecord} from './myMoneyRecord';
   templateUrl: 'form1.component.html'
 })
 
-export class Form1Component{
+export class Form1Component implements OnInit {
+  myMoneyRecords : MyMoneyRecord[] = [];
+  constructor(
+    private myMoneyRecordService:MyMoneyRecordService
+  ){}
 
   myAccTypeList = ['Expense','Income'];
   count:number = 1;
   currDate:Date = new Date();
   today: number = Date.now();
-  model = new MyMoneyRecord(this.count++,0,this.myAccTypeList[0],'2016-01-10');
+  
+  model = new MyMoneyRecord(this.count,0,this.myAccTypeList[0],'2016-01-10');
   submitted = false;
-  myMoneyRecords : MyMoneyRecord[] = [];
+  
   onSubmit(){
     this.submitted = true;
+    console.log("count in before -- "+this.count);
+    this.add(this.count,this.model.usrAmount,this.model.usrAmountType,this.model.usrAmountDate);
     console.log(this.today+"--Form1Component Submitted date - "+this.model.usrAmountDate);
-    this.myMoneyRecords.push(new MyMoneyRecord(
-      this.model.pid,
-      this.model.usrAmount,
-      this.model.usrAmountType,
-      this.model.usrAmountDate
-    ))
+    this.count = this.count +1;
+    console.log("count in submit -- "+this.count);
+    //this.myMoneyRecords.push(new MyMoneyRecord(
+     // this.model.pid,
+     // this.model.usrAmount,
+     // this.model.usrAmountType,
+     // this.model.usrAmountDate
+    //));
   }
   active = true;
+
+  getMyMoneyRecords(): void {
+    this.myMoneyRecordService
+        .getMyMoneyRecords()
+        .then(myMoneyRecords => this.myMoneyRecords = myMoneyRecords);
+  }
+
+  add(pid: number,usrAmount:number,usrAmountType:string,usrAmountDate:string): void {
+    //name = name.trim();
+    //if (!name) { return; }
+    this.myMoneyRecordService.create(pid,usrAmount,usrAmountType,usrAmountDate)
+      .then(hero => {
+        this.myMoneyRecords.push(hero);
+      });
+  }
+
+  ngOnInit(): void {
+    this.getMyMoneyRecords();
+  }
 }
--- app/myMoneyRecord.service.ts
+++ app/myMoneyRecord.service.ts
@@ -7,5 +7,30 @@ import { MyMoneyRecord } from './myMoneyRecord';
 
 @Injectable()
 export class MyMoneyRecordService{
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private myMoneyRecordUrl = 'app/myMoneyRecords';  
+
+    constructor(private http: Http) { }
+
+    getMyMoneyRecords(): Promise<MyMoneyRecord[]> {
+        return this.http.get(this.myMoneyRecordUrl)
+               .toPromise()
+               .then(response => response.json().data as MyMoneyRecord[])
+               .catch(this.handleError);
+    }
+
+    create(pid:number,usrAmount:number,usrAmountType:string,usrAmountDate:string): Promise<MyMoneyRecord> {
+     return this.http
+      .post(this.myMoneyRecordUrl, JSON.stringify({pid:pid,usrAmount:usrAmount,usrAmountType:usrAmountType,usrAmountDate:usrAmountDate}),
+         {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); 
+        return Promise.reject(error.message || error);
+    }
 
 }
